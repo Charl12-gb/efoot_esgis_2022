1edae194d1bd51ad6c4f3d0fff5cccfa151412e8
Charles GBOYOU <gboyoucharles22@hmail.com>
2022-04-17 17:05:20 +0100

    Implementation

--- view/layout/equipes.php
+++ view/layout/equipes.php
@@ -1,22 +1,6 @@
-<div class="lg:container lg:mx-auto mx-auto py-6 sm:px-6 lg:px-8">
-
-    <h2 class="text-2xl font-extrabold text-gray-900">E-FOOT / Equipes</h2>
-    <p>
-        Cliquez sur le nom ou l'image d'un comité pour avoir plus de details.
-    </p>
-    <hr><br>
-    <div class="p-4 w-full text-center bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <h3 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Vote-BDE / Comités</h3>
-        <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Le Bureau des Etudiants d'Esgis (BDE) est un groupe d'étudiants bénévoles qui consacrent une grande partie de leur temps libre à l'organisation de la vie étudiante.
-            Cela passe par l'organisation d'excursions, de soirées, la mise en place des différents club ainsi que la mise en place de plusieurs activités.
-            L'adhésion au bureau est annuelle et se faisait sur vote des responsables uniquement.
-            Mais cette année il a été décidé que tous devrait avoir le droit de vote. C'est dans ce cadre que VOTE-BDE a été mis en place, en vue de faciliter et de permettre a tous les étudiants d'Esgis de faire
-            connaitre leurs voies en votant pour le candidat de leurs choix et ce dans la plus grande transparence. Alors pourquoi ne pas vous inscrire afin d'être au parfum des informations pour au moment venu être apte à voter???
-            <br><strong>Faites nous confiance. </strong>
-        </p>
-        <div class="justify-center items-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-        </div>
-    </div>
+<main class="flex-1 overflow-x-hidden overflow-y-auto">
+    <div class="container mx-auto">
+    <h2 class="text-2xl font-bold text-gray-700 ml-10">Les équipes participantes</h2>
     <div class="pt-6 pb-12 bg-gray-300">
         <div>
             <div class="container w-100 lg:w-4/5 mx-auto flex flex-col">
@@ -85,4 +69,5 @@
            </div>
         </div>
     </div>
-</div>
+    </div>
+</main>
