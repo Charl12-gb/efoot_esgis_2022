18c036cd8d027bdb1859a6ae46b2a650f6f54ad1
Charles GBOYOU <gboyoucharles22@hmail.com>
2022-04-23 18:33:55 +0100

    suite

--- view/layout/accueil.php
+++ view/layout/accueil.php
@@ -6,43 +6,50 @@
         <div class="px-6 py-2">
             <div class="container flex justify-between mx-auto">
                 <div class="w-full lg:w-8/12">
-                    <div class="mt-6">
-                        <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
-                            <div class="mt-2">
-                                <div id="carouselExampleCaptions" class="carousel slide relative" data-bs-ride="carousel">
-                                    <div class="carousel-inner relative w-full overflow-hidden">
-                                        <?php
-                                        foreach (get_match_play() as $key => $value) {
-                                        ?>
-                                            <div class="carousel-item active relative float-left w-full">
-                                                <h2 class="text-2xl font-bold text-gray-700">
-                                                    <h3 class="text-2xl font-semibold"><span class="text-yellow-300"><?= get_equipe_name($value['id_equipe1']) ?></span>
-                                                    <?= ($value['score1']) ?> : <?= ($value['score2']) ?>
-                                                        <span class="text-teal-300"><?= get_equipe_name($value['id_equipe2']) ?></span>
-                                                    </h3>
-                                                </h2>
+                    <?php
+                    $match_play = get_match_play();
+                    if ($match_play != null) {
+                    ?>
+                        <div class="mt-6">
+                            <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
+                                <div class="mt-2">
+                                    <div id="carouselExampleCaptions" class="carousel slide relative" data-bs-ride="carousel">
+                                        <div class="carousel-inner relative w-full overflow-hidden">
+                                            <?php
+                                            foreach ($match_play as $key => $value) {
+                                            ?>
+                                                <div class="carousel-item active relative float-left w-full">
+                                                    <h2 class="text-2xl font-bold text-gray-700">
+                                                        <h3 class="text-2xl font-semibold"><span class="text-yellow-300"><?= get_equipe_name($value['id_equipe1']) ?></span>
+                                                            <?= ($value['score1']) ?> : <?= ($value['score2']) ?>
+                                                            <span class="text-teal-300"><?= get_equipe_name($value['id_equipe2']) ?></span>
+                                                        </h3>
+                                                    </h2>
 
-                                                <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                                    Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
-                                                    reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!
-                                                </p>
-                                            </div>
-                                        <?php
-                                        }
-                                        ?>
+                                                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                                        Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
+                                                        reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!
+                                                    </p>
+                                                </div>
+                                            <?php
+                                            }
+                                            ?>
+                                        </div>
+                                        <button class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                                            <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                                            <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                                            <span class="visually-hidden">Next</span>
+                                        </button>
                                     </div>
-                                    <button class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                                        <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                                        <span class="visually-hidden">Previous</span>
-                                    </button>
-                                    <button class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                                        <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                                        <span class="visually-hidden">Next</span>
-                                    </button>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    <?php
+                    }
+                    ?>
                     <div class="mt-6">
                         <div class="max-w-4xl mx-auto bg-white rounded-lg shadow-md">
                             <div class="flex items-center justify-between">
@@ -74,24 +81,44 @@
                                             <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="<?= $identify ?>" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog">
                                                 <div class="modal-dialog modal-dialog-centered relative w-auto pointer-events-none">
                                                     <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-                                                        <div class="text-center modal-header items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-                                                            <h3 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalScrollableLabel">
-                                                                <span class="text-yellow-300 text-left">
-                                                                    <?= get_equipe_name($value['id_equipe1']) ?>
-                                                                </span> vs
-                                                                <span class="text-teal-300 text-right">
-                                                                    <?= get_equipe_name($value['id_equipe2']) ?>
-                                                                </span>
-                                                            </h3>
-                                                            <button type="button" class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                        </div>
                                                         <div class="modal-body p-4 relative">
-                                                            <p>This is a vertically centered modal.</p>
-                                                        </div>
-                                                        <div class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-                                                            <button type="button" class="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-dismiss="modal">
-                                                                Close
-                                                            </button>
+                                                            <table style="width: 100%; text-align:center">
+                                                                <tr>
+                                                                    <th><?= get_equipe_name($value['id_equipe1']) ?></th>
+                                                                    <th> 0 : 0 </th>
+                                                                    <th><?= get_equipe_name($value['id_equipe2']) ?></th>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>-</td>
+                                                                    <td>Possession de balle</td>
+                                                                    <td>-</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>-</td>
+                                                                    <td>Tir</td>
+                                                                    <td>-</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>-</td>
+                                                                    <td>Tir cadrés</td>
+                                                                    <td>-</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>-</td>
+                                                                    <td>Corners</td>
+                                                                    <td>-</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>-</td>
+                                                                    <td>Arrêts</td>
+                                                                    <td>-</td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td>-</td>
+                                                                    <td>Fautes commises</td>
+                                                                    <td>-</td>
+                                                                </tr>
+                                                            </table>
                                                         </div>
                                                     </div>
                                                 </div>
