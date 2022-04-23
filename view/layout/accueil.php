ccb60ff8283b717569e3fdd2b1ab6d3d44389c54
manlidi <87266496+manlidi@users.noreply.github.com>
2022-04-23 16:52:29 +0100

    Modal

--- view/layout/accueil.php
+++ view/layout/accueil.php
@@ -29,12 +29,49 @@
                                         <?php
                                         $prochaine_matchs = get_prochaine_match(2);
                                         foreach ($prochaine_matchs as $key => $value) {
+                                            $identify = 'modal' . $value['id'];
                                         ?>
-                                            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 text-gray-100 bg-gray-600">
+                                            <a href="#" data-bs-toggle="modal" data-bs-target="#<?= $identify ?>">
+                                            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 text-gray-100 bg-gray-600" >
                                                 <div class="space-y-4">
                                                     <h3 class="text-2xl font-semibold"><span class="text-yellow-300"><?= get_equipe_name( $value['id_equipe1'] ) ?></span> vs <span class="text-teal-300"><?= get_equipe_name( $value['id_equipe2'] ) ?></span></h3>
                                                     <p class="text-[12px]"><?= get_name_capitaine($value['id_equipe1'] ) ?> || <?= get_name_capitaine( $value['id_equipe2'] ) ?></p>
                                                     <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
+                                                </div>
+                                            </div>
+                                            </a>
+                                            <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto" id="<?= $identify ?>" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog">
+                                                <div class="modal-dialog modal-dialog-centered relative w-auto pointer-events-none">
+                                                    <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
+                                                    <div class="text-center modal-header items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
+                                                        <h3 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalScrollableLabel">
+                                                            <span class="text-yellow-300 p-4 text-left">
+                                                                <?= get_equipe_name( $value['id_equipe1'] ) ?>
+                                                            </span> vs
+                                                            <span class="text-teal-300 p-4 text-right">
+                                                                <?= get_equipe_name( $value['id_equipe2'] ) ?>
+                                                            </span>
+                                                        </h3>
+                                                        <button type="button"
+                                                        class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                                                        data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body relative p-4">
+                                                        <p>This is a vertically centered modal.</p>
+                                                    </div>
+                                                    <div
+                                                        class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
+                                                        <button type="button"
+                                                        class="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+                                                        data-bs-dismiss="modal">
+                                                        Close
+                                                        </button>
+                                                        <button type="button"
+                                                        class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1">
+                                                        Save changes
+                                                        </button>
+                                                    </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         <?php
@@ -181,4 +218,4 @@
             </div>
         </div>
     </div>
-</main>
+ </main>
