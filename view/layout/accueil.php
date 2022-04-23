4ed51318f22384b8175d1921326cdc6e59963389
Charles GBOYOU <gboyoucharles22@hmail.com>
2022-04-23 17:37:52 +0100

    suite

--- view/layout/accueil.php
+++ view/layout/accueil.php
@@ -17,7 +17,7 @@
                                             <div class="carousel-item active relative float-left w-full">
                                                 <h2 class="text-2xl font-bold text-gray-700">
                                                     <h3 class="text-2xl font-semibold"><span class="text-yellow-300"><?= get_equipe_name($value['id_equipe1']) ?></span>
-                                                        3 : 1
+                                                    <?= ($value['score1']) ?> : <?= ($value['score2']) ?>
                                                         <span class="text-teal-300"><?= get_equipe_name($value['id_equipe2']) ?></span>
                                                     </h3>
                                                 </h2>
@@ -64,7 +64,10 @@
                                                         vs
                                                         <h3 class="text-2xl font-semibold"><span class="text-teal-300"><?= get_equipe_name($value['id_equipe2']) ?></span></h3>
                                                         <hr>
-                                                        <p class="text-[12px] font-bold pt-5"><?= format_date($value['rencontre_date'])  ?></p>
+                                                        <p class="text-[12px] font-bold pt-5">
+                                                            <?= format_date($value['rencontre_date'])  ?><br>
+                                                            Lieu : <?= ($value['lieu_rencontre'])  ?>
+                                                        </p>
                                                     </div>
                                                 </div>
                                             </a>
@@ -73,24 +76,21 @@
                                                     <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
                                                         <div class="text-center modal-header items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
                                                             <h3 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalScrollableLabel">
-                                                                <span class="text-yellow-300 p-4 text-left">
+                                                                <span class="text-yellow-300 text-left">
                                                                     <?= get_equipe_name($value['id_equipe1']) ?>
                                                                 </span> vs
-                                                                <span class="text-teal-300 p-4 text-right">
+                                                                <span class="text-teal-300 text-right">
                                                                     <?= get_equipe_name($value['id_equipe2']) ?>
                                                                 </span>
                                                             </h3>
                                                             <button type="button" class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline" data-bs-dismiss="modal" aria-label="Close"></button>
                                                         </div>
-                                                        <div class="modal-body relative p-4">
+                                                        <div class="modal-body p-4 relative">
                                                             <p>This is a vertically centered modal.</p>
                                                         </div>
                                                         <div class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
                                                             <button type="button" class="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out" data-bs-dismiss="modal">
                                                                 Close
-                                                            </button>
-                                                            <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ml-1">
-                                                                Save changes
                                                             </button>
                                                         </div>
                                                     </div>
