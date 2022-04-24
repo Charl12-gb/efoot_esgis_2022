fdee84e025d80cdd2bb92e5f914b4345c55b84c1
Charles GBOYOU <gboyoucharles22@hmail.com>
2022-04-24 20:32:29 +0100

    Connexion + Ajax

--- view/partie/footer.php
+++ view/partie/footer.php
@@ -2,5 +2,7 @@
             </div>
         </div>
     </div>
+    <script src="https://unpkg.com/@themesberg/flowbite@1.3.0/dist/flowbite.bundle.js"></script>
+    <script src="../../modele/admin/js.js"></script>
 </body>
 </html>
